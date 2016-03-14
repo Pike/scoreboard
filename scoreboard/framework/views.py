4b308f89b965a24419461a392d8cd1495b617161
Axel Hecht <axel@pike.org>
2016-03-14 12:43:14 +0100

    some rough homepage view

--- scoreboard/framework/views.py
+++ scoreboard/framework/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.db.models import Count
+
+from mqm_base.models import Locale
 
 def index(request):
-    return render(request, 'framework/index.html')
+    locales = (Locale.objects
+        .order_by('name')
+        .annotate(issues_count=Count('issues')))
+    return render(request, 'framework/index.html', {
+        "locales": locales
+    })
