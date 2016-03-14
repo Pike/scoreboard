7687c6a94db2bd073a83cb9204af9f5e44428d62
Axel Hecht <axel@pike.org>
2016-03-14 17:58:07 +0100

    show more entries, css fixes

--- mqm_base/views.py
+++ mqm_base/views.py
@@ -5,7 +5,7 @@ from mqm_base.models import Locale, Issue
 def locale_issues(request, locale_code):
     locale = get_object_or_404(Locale, code=locale_code)
     issues = (Issue.objects
-        .filter(locale=locale))[:20]
+        .filter(locale=locale))[:100]
     return render(request, 'mqm_base/locale-issues.html', {
         "locale": locale,
         "issues": issues
